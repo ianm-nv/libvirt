f3e8d953db3cbf7eafe0e5238b7bd816dfbc3720
Ian May <ianm@nvidia.com>
2025-03-10 20:36:29 +0000

    qemu: Add cgroup and namespace setup for ACPI EGM memory device
    Signed-off-by: Ian May <ianm@nvidia.com>

--- src/qemu/qemu_namespace.c
+++ src/qemu/qemu_namespace.c
@@ -677,6 +677,24 @@ qemuDomainSetupLaunchSecurity(virDomainObj *vm,
 
 
 static int
+qemuDomainSetupAcpiEgm(virDomainObj *vm,
+                       GSList **paths)
+{
+    virDomainAcpiEgmDef *egm = vm->def->egm;
+    g_autofree char *path = NULL;
+
+    if (!egm)
+        return 0;
+
+    path = g_strdup_printf("/dev/%s", egm->alias);
+
+    *paths = g_slist_prepend(*paths, g_steal_pointer(&path));
+
+    return 0;
+}
+
+
+static int
 qemuNamespaceMknodPaths(virDomainObj *vm,
                         GSList *paths,
                         bool *created);
@@ -727,6 +745,9 @@ qemuDomainBuildNamespace(virQEMUDriverConfig *cfg,
         return -1;
 
     if (qemuDomainSetupLaunchSecurity(vm, &paths) < 0)
+        return -1;
+
+    if (qemuDomainSetupAcpiEgm(vm, &paths) < 0)
         return -1;
 
     if (qemuNamespaceMknodPaths(vm, paths, NULL) < 0)
