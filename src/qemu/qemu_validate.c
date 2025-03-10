8df17f1002b6045826b006846453ac07b4d2ea1d
Ian May <ianm@nvidia.com>
2025-03-10 20:34:55 +0000

    qemu: Add validation logic for ACPI EGM memory device
    Signed-off-by: Ian May <ianm@nvidia.com>

--- src/qemu/qemu_validate.c
+++ src/qemu/qemu_validate.c
@@ -4863,6 +4863,48 @@ qemuValidateDomainDeviceDefPstore(virDomainPstoreDef *pstore,
     return 0;
 }
 
+static int
+qemuValidateDomainDeviceDefAcpiEgm(virDomainAcpiEgmDef *egm,
+                                  const virDomainDef *def,
+                                  virQEMUCaps *qemuCaps)
+{
+    g_autofree char *egmPath = NULL;
+
+    if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_DEVICE_ACPI_EGM_MEMORY)) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                      _("ACPI EGM memory device is not supported with this QEMU binary"));
+        return -1;
+    }
+
+    /* Validate NUMA node if configured */
+    if (def->numa && egm->node >= 0) {
+        if (egm->node >= virDomainNumaGetNodeCount(def->numa)) {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                          _("NUMA node %1$d for EGM device does not exist"),
+                          egm->node);
+            return -1;
+        }
+    }
+
+    /* Validate EGM device path exists and is accessible */
+    egmPath = g_strdup_printf("/dev/%s", egm->alias);
+    if (!virFileExists(egmPath)) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                      _("EGM device path '%1$s' does not exist"),
+                      egmPath);
+        return -1;
+    }
+
+    /* Check if we have proper permissions */
+    if (access(egmPath, R_OK | W_OK) < 0) {
+        virReportSystemError(errno,
+                          _("Cannot access EGM device '%1$s'"),
+                          egmPath);
+        return -1;
+    }
+
+    return 0;
+}
 
 static int
 qemuSoundCodecTypeToCaps(int type)
@@ -5604,6 +5646,9 @@ qemuValidateDomainDeviceDef(const virDomainDeviceDef *dev,
 
     case VIR_DOMAIN_DEVICE_PSTORE:
         return qemuValidateDomainDeviceDefPstore(dev->data.pstore, def, qemuCaps);
+
+    case VIR_DOMAIN_DEVICE_EGM:
+        return qemuValidateDomainDeviceDefAcpiEgm(dev->data.egm, def, qemuCaps);
 
     case VIR_DOMAIN_DEVICE_LEASE:
     case VIR_DOMAIN_DEVICE_PANIC:
